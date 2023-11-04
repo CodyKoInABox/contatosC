ba4c76f4049a429836ee84431709d99d51562ee7
CodyKoInABox <codykoinabox@gmail.com>
2023-11-04 17:09:33 -0300

    menu 100%

--- main.c
+++ main.c
@@ -10,6 +10,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+void mainMenu();
+
 typedef char String[100];
 
 // Estrutura de uma node da lista
@@ -120,28 +122,26 @@ void deletarContatoNome(String nome){
     if(currentNode->nextNode == NULL && currentNode->previousNode == NULL){
         // head vira null
         head = NULL;
-        printf("primeiro");
+        
     }
     // se for a ultima node
     else if(currentNode->nextNode == NULL){
         // node anterior aponta para null
         currentNode->previousNode->nextNode = NULL;
-        printf("segundo");
+
     }
     // se for a primeira node
     else if(currentNode->previousNode == NULL){
         // head aponta para a proxima node e proxima node aponta para head
         head = currentNode->nextNode;
         currentNode->nextNode->previousNode = head;
-        printf("terceiro");
-        
+          
     }
     // se estiver no meio
     else{
         // node anterior aponta para a proxima node e proxima node aponta para node anterior
         currentNode->previousNode->nextNode = currentNode->nextNode;
         currentNode->nextNode->previousNode = currentNode->previousNode;
-        printf("quarto");
     }
 
     
@@ -167,46 +167,219 @@ void listarContatos(){
         printNode(currentNode);
         currentNode = currentNode->nextNode;
     }
+
+    return;
     
 }
+
+void criarContatoMenu(){
+
+    Node newContato;
+
+    printf("CRIAR CONTATO");
+    printf("\n");
+    printf("-------------");
+    printf("\n");
+    printf("Insira o nome: ");
+    gets(newContato.nome);
+    printf("Insira o DDD: ");
+    scanf("%d", &newContato.ddd);
+    fflush(stdin);
+    printf("Insira o numero: ");
+    scanf("%d", &newContato.numero);
+    fflush(stdin);
+
+    criarContato(&newContato);
+
+    mainMenu();
+
+}
+
+
+void listarContatosMenu(){
+
+    listarContatos();
+
+    printf("\n");
+    printf("Pressione alguma tecla para voltar ao menu: ");
+    getchar();
+    mainMenu();
+
+}
+
+void buscarContatoMenu(){
+
+    int buscarOp;
+
+    printf("BUSCAR CONTATO");
+    printf("\n");
+    printf("--------------");
+    printf("\n");
+    printf("1 -> Buscar nome");
+    printf("\n");
+    printf("2 -> Buscar numero");
+    printf("\n");
+    printf("Insira sua opcao: ");
+    scanf("%d", &buscarOp);
+    fflush(stdin);
+
+    system("cls");
+
+    String nome;
+    int numero;
+
+
+
+    switch(buscarOp){
+        // buscar nome
+        case 1:
+            printf("BUSCAR NOME");
+            printf("\n");
+            printf("--------------");
+            printf("\n");
+            printf("Insira o nome: ");
+            gets(nome);
+            system("cls");
+            printNode(lerContatoNome(nome));
+            printf("\n");
+            printf("Pressione alguma tecla para voltar ao menu: ");
+            getchar();
+            mainMenu();
+        break;
+        // buscar numero
+        case 2:
+        
+            printf("BUSCAR NUMERO");
+            printf("\n");
+            printf("--------------");
+            printf("\n");
+            printf("Insira o numero: ");
+            scanf("%d", &numero);
+            fflush(stdin);
+            system("cls");
+            printNode(lerContatoNumero(numero));
+            printf("\n");
+            printf("Pressione alguma tecla para voltar ao menu: ");
+            getchar();
+            mainMenu();
+
+        break;
+
+        default:
+            buscarContatoMenu();
+        break;
+    }
+}
+
+void atualizarContatoMenu(){
+
+    String nome;
+    int ddd;
+    int numero;
+
+    printf("ATUALIZAR CONTATO");
+    printf("\n");
+    printf("-------------");
+    printf("\n");
+    printf("Insira o nome do contato: ");
+    gets(nome);
+    printf("Insira o novo DDD: ");
+    scanf("%d", &ddd);
+    fflush(stdin);
+    printf("Insira o novo numero: ");
+    scanf("%d", &numero);
+    fflush(stdin);
+
+    atualizarContatoNome(nome, numero, ddd);
+
+    printf("\n");
+    printf("Pressione alguma tecla para voltar ao menu: ");
+    getchar();
+    mainMenu();
+
+}
+
+void deletarContatoMenu(){
+
+    String nome;
+
+    printf("DELETAR CONTATO");
+    printf("\n");
+    printf("-------------");
+    printf("\n");
+    printf("Insira o nome do contato: ");
+    gets(nome);
+
+    deletarContatoNome(nome);
+
+    printf("\n");
+    printf("Pressione alguma tecla para voltar ao menu: ");
+    getchar();
+    mainMenu();
+
+}
+
 
 void mainMenu(){
     int op;
     
+    system("cls");
+
     printf("|-- GERENCIADOR DE CONTATOS --|");
+    printf("\n");
     printf("-------------------------------");
+    printf("\n");
     printf("| 1 -> Listar Contatos");
+    printf("\n");
     printf("| 2 -> Buscar Contato");
+    printf("\n");
     printf("| 3 -> Criar Contato");
+    printf("\n");
     printf("| 4 -> Atualizar Contato");
+    printf("\n");
     printf("| 5 -> Deletar Contato");
+    printf("\n");
     printf("| 6 -> Sair");
+    printf("\n");
     printf("-------------------------------");
+    printf("\n");
     printf("Insira a opcao: ");
     
     scanf("%d", &op);
+    fflush(stdin);
     
-    //system(“clear”);
-    system.cls();
+    system("cls");
     
     switch(op){
+
+        // listar contatos
         case 1:
-        
+            listarContatosMenu();
         break;
+
+        // buscar contato
         case 2:
-        
+            buscarContatoMenu();
         break;
+
+        // criar contato
         case 3:
-        
+            criarContatoMenu();
         break;
+
+        // atualizar contato
         case 4:
-        
+            atualizarContatoMenu();
         break;
+
+        // deletar contato
         case 5:
-        
+            deletarContatoMenu();
         break;
+
+        // sair
         default:
-        
+            exit(1);
         break;
     }
 }
